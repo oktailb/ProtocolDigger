c544100002206d7657b5992639a05d270f9894e3
LECOQ Vincent <vincent.lecoq@gmail.com>
2025-01-05 19:55:43 +0900

    Complete redraw of all instruments in timer event

--- cockpit/src/instruments.cpp
+++ cockpit/src/instruments.cpp
@@ -30,6 +30,13 @@ void Instruments::timerEvent(QTimerEvent *event)
     QMainWindow::timerEvent(event);
     ui->centralwidget->repaint();
     ui->EADI->redraw();
+    ui->EHSI->redraw();
+    ui->AI->redraw();
+    ui->ALT->redraw();
+    ui->HI->redraw();
+    ui->TC->redraw();
+    ui->VSI->redraw();
+    ui->ASI->redraw();
 }
 
 template<typename T> T extractVarFromData(PacketData &data, varDef_t &var)
@@ -52,16 +59,15 @@ void Instruments::play()
 
         PacketData data;
         queue.pop(data, std::chrono::milliseconds(200));
-        double altitude = 0.0;
         double timestamp0 = data.ts.tv_sec * 1000000 + data.ts.tv_usec;
         while (run) {
             run = queue.pop(data, std::chrono::milliseconds(200));
             double timestamp = data.ts.tv_sec * 1000000 + data.ts.tv_usec;
             if (run)
             {
-                altitude = extractVarFromData<double>(data, variables["altitude"]);
-                ui->EADI->setAltitude(altitude);
-                altitude += 0.1;
+                ui->EADI->setAltitude(extractVarFromData<double>(data, variables["altitude"]));
+                ui->ALT->setAltitude(extractVarFromData<double>(data, variables["altitude"]));
+
                 usleep(timestamp - timestamp0);
                 timestamp0 = timestamp;
             }
